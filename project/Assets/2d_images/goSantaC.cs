11a43e6a0f9024629ae8ae20f164bd67a2a0d8eb
Ben McNelly <benmcnelly@gmail.com>
2013-12-03 20:09:39 -0600

    Init

---
+++ project/Assets/2d_images/goSantaC.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class goSantaC : MonoBehaviour {
+	void Update() {
+		int fingerCount = 0;
+		foreach (Touch touch in Input.touches) {
+			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+				fingerCount++;
+			Debug.Log (fingerCount);
+		}
+		
+		if (fingerCount == 1)
+		{
+			Debug.Log("There is " + fingerCount + " finger touching the screen");
+			rigidbody2D.AddForce(Vector3.right * 35);
+		}else if (fingerCount ==2)
+		{
+			Debug.Log("There is " + fingerCount + " fingers touching the screen");
+			rigidbody2D.AddForce(Vector3.up * 25);
+		}else if (fingerCount ==3)
+		{
+			Debug.Log("There is " + fingerCount + " fingers touching the screen");
+			rigidbody2D.AddForce(Vector3.left * 25);
+		}
+	}
+}
