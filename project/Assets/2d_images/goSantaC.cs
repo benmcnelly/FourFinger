eec4092ceff539fc2883fa4a06891dbe27c3bd6f
Ben McNelly <benmcnelly@gmail.com>
2013-12-16 23:08:29 -0600

    Added in some sprites to show flames on the side that is getting power

--- project/Assets/2d_images/goSantaC.cs
+++ project/Assets/2d_images/goSantaC.cs
@@ -10,22 +10,37 @@ public class goSantaC : MonoBehaviour {
 				fingerCount++;
 			Debug.Log (fingerCount);
 		}
-		
+		if (fingerCount == 0)
+		{
+			GameObject.Find("forwardrocket").renderer.enabled = false;
+			GameObject.Find("downrocket").renderer.enabled = false;
+			GameObject.Find("updrocket").renderer.enabled = false;
+			GameObject.Find("backrocket").renderer.enabled = false;
+		}
 		if (fingerCount == 1)
 		{
 			Debug.Log("There is " + fingerCount + " finger touching the screen");
 			Vector2 v2Force = 35 * transform.right;
 			rigidbody2D.AddForce(v2Force);
+			GameObject.Find("forwardrocket").renderer.enabled = true;
 		}else if (fingerCount ==2)
 		{
 			Debug.Log("There is " + fingerCount + " fingers touching the screen");
 			Vector2 v2Forceup = 35 * transform.up;
 			rigidbody2D.AddForce(v2Forceup);
+			GameObject.Find("uprocket").renderer.enabled = true;
 		}else if (fingerCount ==3)
 		{
 			Debug.Log("There is " + fingerCount + " fingers touching the screen");
 			Vector2 v2Forceleft = -35 * transform.right;
 			rigidbody2D.AddForce(v2Forceleft);
+			GameObject.Find("backrocket").renderer.enabled = true;
+		}else if (fingerCount ==4)
+		{
+			Debug.Log("WOAH 4 Fingers");
+			Vector2 v2Forcetop = -35 * transform.up;
+			rigidbody2D.AddForce(v2Forcetop);
+			GameObject.Find("downrocket").renderer.enabled = true;
 		}
 	}
 }
---
+++ project/Assets/follow.js
@@ -0,0 +1,14 @@
+﻿var dampTime : float = 0.3; //offset from the viewport center to fix damping
+private var velocity = Vector3.zero;
+var target : Transform;
+ 
+function Update() {
+    if(target) {
+        var point : Vector3 = camera.WorldToViewportPoint(target.position);
+        var delta : Vector3 = target.position -
+                    camera.ViewportToWorldPoint(Vector3(0.5, 0.5, point.z));
+        var destination : Vector3 = transform.position + delta;
+        transform.position = Vector3.SmoothDamp(transform.position, destination, 
+                                                velocity, dampTime);
+    }
+}
